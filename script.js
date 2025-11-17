ee0538dd516b3496a85b3529ea90399a0cfc421c
chr0606 <cherie.illust0606@gmail.com>
2025-11-17 21:25:29 +0900

    Implement hamburger menu toggle in script.js
    Add hamburger menu toggle functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('navMenu');
+hamburger.addEventListener('click', () => {
+  if(navMenu.style.display === 'block') {
+     navMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'block';
+  }
+});
