b8297f2ba638cd8170a4fcbfbcecd3f447175a42
chr0606 <cherie.illust0606@gmail.com>
2025-11-19 00:45:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-sp');
 
 button.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+  nav.classList.toggle('open');
 });
