63ac89edfa6085a4f7539b8034bb55ddc14e7f78
chr0606 <cherie.illust0606@gmail.com>
2025-11-19 16:46:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const nav = document.querySelector('.nav-sp');
 
 button.addEventListener('click', () => {
   nav.classList.toggle('open');
+
+  if (nav.classList.contains('open')) {
+    button.textContent = '✕';
+     // 背景固定
+    document.body.classList.add('no-scroll');
+  } else {
+    button.textContent = '≡';
+    // 背景スクロール解除
+    document.body.classList.remove('no-scroll');
+  }
 });
